c5d5ed8cc7c8e40ef5b299c20b35cc27b8871271
Maryna Melnyk <melnikm535@gmail.com>
2025-06-21 22:06:47 +0300

    add links to projects

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -6,16 +6,20 @@ function ProjectItem({item}) {
 
   return (
     <>
-        <div className='pr-item'>
+        <div className='pr-item' onClick={() => window.open(item.github, '_blank', 'noopener,noreferrer')}>
             <div className='pr-ph-section'>
                 <img className='pr-photo' src={item.image} />
             </div>
 
             <div className='pr-tx-section'>
                 <h3>{item.title}</h3>
-                {item.demo && 
-                    <a className='pr-link-demo' href={item.demo} target="_blank">Live demo 🡥</a>
-                }
+                <div className='pr-links'>
+                    {item.demo && 
+                        <a className='pr-link-demo' href={item.demo} target="_blank">Live demo 🡥</a>
+                    }
+                    <a className='pr-link-demo' href={item.github} target="_blank">GitHub 🡥</a>
+                </div>
+                    
                 <p className='pr-description'>
                     {item.description}
                 </p>
