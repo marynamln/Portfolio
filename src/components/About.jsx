73c52c0115fc36f9675b98a7d47aa653aff4da14
Maryna Melnyk <melnikm535@gmail.com>
2025-06-25 16:14:58 +0300

    update information about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ function About() {
       <section id="about" className='about'>
         <div className='about-tx'>
             <h1>Maryna Melnyk</h1>
-            <h3>Front End Developer</h3>
+            <h3>Front End Developer (junior)</h3>
             <p className='about-descript'>I am a 4th year Software Engineering student at the National University of Kyiv-Mohyla Academy, focusing on front-end web development. I enjoy creating clean, responsive, and accessible user interfaces, and I constantly strive to improve both my technical and design skills.</p>
             <p className='about-descript'>I have hands-on experience with React, SCSS, and RESTful APIs, and I pay special attention to usability and layout quality. I'm looking to become part of a friendly and professional team where I can grow as a developer, contribute to meaningful projects, and learn from others.</p>
         </div>
